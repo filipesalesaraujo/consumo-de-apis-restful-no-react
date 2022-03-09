9fbcbb7a8bfd3e9b61fec92f298fcd620ccb0012
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-03-09 18:57:47 -0300

    hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useFetch } from "./hooks/useFetch";
 
 type Repository = {
   full_name: string;
@@ -7,19 +6,12 @@ type Repository = {
 };
 
 function App() {
-  const [repositories, setRepositories] = useState<Repository[]>([]);
-
-  useEffect(() => {
-    axios
-      .get("https://api.github.com/users/filipesalesaraujo/repos")
-      .then((response) => {
-        setRepositories(response.data);
-      });
-  }, []);
-
+  const { data: repositories } = useFetch<Repository[]>(
+    "https://api.github.com/users/filipesalesaraujo/repos"
+  );
   return (
     <ul>
-      {repositories.map((repo) => {
+      {repositories?.map((repo) => {
         return (
           <li key={repo.full_name}>
             <strong>{repo.full_name}</strong>
